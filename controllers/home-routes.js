992e893c192bdb186f99b75571e41ea33206cf05
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-04-01 02:47:53 -0400

    Game-Library-inProgress

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
+router.get('/games', withAuth, async (req, res) => {
+  res.render('gamelibrary');
+});
+
+router.get('/addgame', withAuth, async (req, res) => {
+  res.render('addgame');
+});
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
