7f1389cacf8d486d3622b7ba5253c7d65378892d
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-04-04 16:07:15 -0400

    Updated CSS

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,16 +1,16 @@
-async function newFormHandler(event) {
+async function profileFormHandler(event) {
   event.preventDefault();
 
-  const firstname = document.querySelector("#first_name").value;
-  const lastname = document.querySelector("#last_name").value;
-  const aboutme = document.querySelector("#about_me").value;
-  const usernamepsn = document.querySelector("#psn_name").value;
-  const usernamexbox = !!document.querySelector("#xbox_name");
-  const usernamesteam = !!document.querySelector("#steam_name");
-  const usernamenintendo = !!document.querySelector("#nintendo_name");
+  const firstname = document.querySelector('#first_name').value;
+  const lastname = document.querySelector('#last_name').value;
+  const aboutme = document.querySelector('#about_me').value;
+  const usernamepsn = document.querySelector('#psn_name').value;
+  const usernamexbox = !!document.querySelector('#xbox_name');
+  const usernamesteam = !!document.querySelector('#steam_name');
+  const usernamenintendo = !!document.querySelector('#nintendo_name');
 
   const response = await fetch(`/api/profile`, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       firstname,
       lastname,
@@ -21,17 +21,17 @@ async function newFormHandler(event) {
       usernamenintendo,
     }),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 
   if (response.ok) {
-    document.location.replace("/landing");
+    document.location.replace(`/landing`);
   } else {
-    alert("Failed to post profile");
+    alert('Failed to post profile');
   }
 }
 
 document
-  .querySelector(".profile-form")
-  .addEventListener("submit", newFormHandler);
+  .querySelector('.profile-form')
+  .addEventListener('submit', profileFormHandler);
